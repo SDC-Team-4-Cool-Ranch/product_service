18ad4e3b1b9333ec3b6d2be589a7ca1884a6b876
David Zhang <david.zny1022@gmail.com>
2023-02-16 00:41:30 -0800

    docker set up

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,3 @@
 const router = require('express').Router();
-const pool = require('../database/index');
-
-router.get('/', (req, res) => {
-  // testing db connection
-  pool.connect((err, client) => {
-    client.query('SELECT * FROM accounts');
-  });
-});
 
 module.exports = router;
